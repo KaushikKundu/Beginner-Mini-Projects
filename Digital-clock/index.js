ae47fc9d8209e8b38122a0f4279284b0fae75d32
Kaushik Kundu <kaushikkundu774@gmail.com>
2024-02-01 22:06:14 +0530

    digital clock

---
+++ Digital-clock/index.js
@@ -0,0 +1,23 @@
+const clock = document.querySelector(".clock");
+const hour = document.querySelector("#hr");
+const minutes = document.querySelector("#min");
+const second = document.querySelector("#sec");
+const ampm = document.querySelector("#ampm");
+
+setInterval(() => {
+  showTime();
+}, 1000);
+
+function showTime() {
+  let curTime = new Date();
+  let hr = curTime.getHours();
+  let sec = curTime.getSeconds();
+  let min = curTime.getMinutes();
+  let ampm = curTime.getHours() >= 12 ? "" : "AM";
+  hr = hr % 12;
+  hour.textContent = hr;
+  minutes.textContent = min;
+  second.textContent = sec;
+  ampm.textContent = ampm;
+}
+
